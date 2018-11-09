c5f0f2ec88cb1fbdb27450104e9ba659a082c50c
dollin <neil.dolling@gmail.com>
2018-11-09 19:41:50 +0000

    #1 selection sort

--- elvaston-algorithms/src/main/java/org/elvaston/sort/SelectionSort.java
+++ elvaston-algorithms/src/main/java/org/elvaston/sort/SelectionSort.java
@@ -7,11 +7,12 @@ import org.slf4j.LoggerFactory;
 import java.util.stream.IntStream;
 
 /**
+ * Selection Sort.
  * One of the simplest sorting algorithms that works as follows:
- *   First, find the smallest item in the array, and exchange it with the first entry. Then, find the next smallest
- *   item and exchange it with the second entry. Continue in this way until the entire array is sorted.
- *   This method is called selection sort because it works by repeatedly selecting the smallest remaining item.
- *   Proposition: Selection sort uses ~(n * n)/2 compares and n exchanges to sort an array of length n.
+ * First, find the smallest item in the array, and exchange it with the first entry. Then, find the next smallest
+ * item and exchange it with the second entry. Continue in this way until the entire array is sorted.
+ * This method is called selection sort because it works by repeatedly selecting the smallest remaining item.
+ * Proposition: Selection sort uses ~(n * n)/2 compares and n exchanges to sort an array of length n.
  */
 public class SelectionSort implements Sort {
 
